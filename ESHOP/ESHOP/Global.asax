0e915e62fac545809e615c4f099cc976aee05b92
DESKTOP-6J0ROFG\TONTRUONG <tonvantruong@codelovers.vn>
2017-03-21 13:52:08 +0700

    Create Project

---
+++ ESHOP/ESHOP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ESHOP.MvcApplication" Language="C#" %>
---
+++ ESHOP/ESHOP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ESHOP.Startup))]
+namespace ESHOP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
